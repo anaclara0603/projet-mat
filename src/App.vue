be7d1f5aed406bfd484025e0511c105fb349b670
= <=>
2023-06-19 16:57:20 -0300

    funciona

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
+import { ref } from 'vue'
+
+const diferente = ref(false)
+const igual = ref(false)
 const matriz1 = [
-  [1, 2, 3],
+  [5, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
 ]
@@ -9,32 +13,28 @@ const matriz2 = [
   [4, 5, 6],
   [3, 6, 7]
 ]
-console.log(matriz1[0])
 
 function compara() {
-  
-  if (matriz1.length == matriz2.length) {
-   
-    for(let a of matriz1[i]){
-      for(let b of matriz2[i]){ 
-        if(a === b){console.log('elemento igaul')}
+  if (
+    matriz1[0].length == matriz2[0].length &&
+    matriz1[1].length == matriz2[1].length &&
+    matriz1[2].length == matriz2[2].length
+  ) {
+    for (let i = 0; i < matriz1[0].length; i++) {
+      for (let j = 0; j < matriz2.length; j++) {
+        if (matriz1[i][j] !== matriz2[i][j]) return false, alert('São diferentes')
       }
+      return true, alert('São iguais')
     }
- 
   }
-  else alert('As matrizes sao diferentes')
-}
-
-function passamatriz(){
-  let i = 0
-  for (i = 0; i < 2; i++) {
-    
-  }
-}
+} 
 </script>
 
 <template>
   <div>
     <button @click="compara()">claudio</button>
+    <div class="igual" v-if="compara">MATRIZES IGUAIS</div>
+    <div class="diferente" v-else-if="compara = false">MATRIZES DIFERENTES</div>
+    <div v-else>PRIMEIRO COMPARE AS MATRIZES</div>
   </div>
 </template>
