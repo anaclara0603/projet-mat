7056c07253f31a589917b8e75a8e7ff5f5e6185e
= <=>
2023-06-20 11:49:40 -0300

    final

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <script setup>
-import { ref } from 'vue'
-
-//const diferente = ref(false)
-//const igual = ref(false)
 const matriz1 = [
   [1, 2, 3],
   [4, 5, 6],
@@ -13,7 +9,6 @@ const matriz2 = [
   [4, 5, 6],
   [3, 6, 7]
 ]
-
 
 function compara() {
   if (
@@ -38,7 +33,67 @@ function compara() {
 </script>
 
 <template>
-  <div>
+    <div class="matrizes">
+      <div class="matriz1">
+        <h2>Matriz 1: </h2>
+        <p>{{ matriz1[0] }}</p>
+        <p>{{ matriz1[1] }}</p>
+        <p>{{ matriz1[2] }}</p>
+      </div>
+      <img src="https://i.pinimg.com/564x/78/46/a8/7846a88f8efa7f880b5b9abea3f02a30.jpg" alt="">
+      <div class="matriz2">
+        <h2>Matriz 2: </h2>
+        <p>{{ matriz2[0] }}</p>
+        <p>{{ matriz2[1] }}</p>
+        <p>{{ matriz2[2] }}</p>      
+      </div>
+    </div>
     <button @click="compara()">claudio</button>
-  </div>
+
 </template>
+
+<style scoped>
+  .matrizes{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    font-family: Arial;
+    font-size: large;
+  }
+  .matriz1{
+    background-color: black;
+    color: white;
+    border-radius: 15px;
+    width: 200px;
+    height: 200px;
+    padding: 10px;
+    box-shadow: 3px 3px 5px gray;
+  }
+  .matriz2{
+    background-color: black;
+    color: white;
+    border-radius: 15px;
+    width: 200px;
+    height: 200px;
+    padding: 10px;
+    box-shadow: 3px 3px 5px gray;
+  }
+  button{
+    display: flex;
+    margin: 10px auto;
+    padding: 10px;
+    border-radius: 15px;
+    color: white;
+    background-color: black;
+    border: 1px solid black;
+    font-size: larger;
+    box-shadow: 3px 3px 5px gray;
+  }
+  button:hover{
+    background-color: rgb(66, 66, 66);
+  }
+  img{
+
+    border-radius: 15px;
+  }
+</style>
